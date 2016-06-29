ad418ea6e7e88c82171132fa941d29ffee065f81
Harp1e <github@bplayer.waitrose.com>
2016-06-30 00:18:54 +0100

    course update to camera gimble

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+
+    private Camera camera;
+    private float rotationSpeed = 2.0f;
+
+    // Use this for initialization
+    void Start () {
+        camera = GetComponentInChildren<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed;
+        transform.localRotation = transform.localRotation * Quaternion.Euler(0, mouseX, 0);
+        camera.transform.localRotation = camera.transform.localRotation * Quaternion.Euler(-mouseY, 0, 0);
+    }
+}
