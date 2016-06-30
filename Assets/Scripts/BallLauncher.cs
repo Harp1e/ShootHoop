fc661f6635e97ff9739b458e13154252615f61f5
Harp1e <github@bplayer.waitrose.com>
2016-06-30 17:54:19 +0100

    added start and game over splash screens

---
+++ Assets/Scripts/BallLauncher.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallLauncher : MonoBehaviour {
+
+	public GameObject ballPrefab;
+	public float ballSpeed = 5.0f;
+
+	private Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = GetComponentInChildren<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+            GameObject instance = Instantiate(ballPrefab);
+            instance.transform.position = camera.transform.position;
+            Rigidbody rb = instance.GetComponent<Rigidbody>();
+            rb.velocity = camera.transform.rotation * Vector3.forward * ballSpeed;
+        }
+	}
+}
